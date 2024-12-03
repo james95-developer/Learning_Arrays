ab1711664f82a14f7fcd0db510c1dbc1d25689c0
james95-developer <alosimiruka@gmail.com>
2024-12-03 17:08:34 +0300

    Learning arrays

---
+++ arrays.js
@@ -0,0 +1,14 @@
+//Array defination
+const myArray = [
+    "Volvo",
+    "Mazda", 
+    "Toyota", 
+    "isuzu"];
+    //Accessing an Array with index 2 position
+let access_array = myArray[2];
+//Changing an Array Element---Toyota will be changed to BMW because it's index is 2
+myArray[2] = "BMW";
+//The toString() method returns an array as a comma separated string:
+myArray.toString(myArray);
+console.log(myArray.toString(myArray));
+
